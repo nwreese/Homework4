5e368af0d615ead4e6c1a79e35fb087d4b8ec084
Nick Reese <74203688+nwreese@users.noreply.github.com>
2020-12-17 16:58:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// created js page to link
+const startElement = document.querySelector("#start");
+const score = document.querySelector("#final-score");
+const timer = document.querySelector(".timer");
+const timeLeft = parseInt(timer.textContent)*60;
+
+
+function startTimer(){
+    interval = setInterval(() => {
+        timeLeft
+    }
+}
+
+// window.prompt("default text");
+startElement.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("What is a web API?");
+    const response = ""
+}
+
+localStorage.getItem("score");
+localStorage.setItem("final-score");
+
+
+
+
+ startElement.addEventListener("click", startElement);
+ timer.addEventListener("click", startTimer);
